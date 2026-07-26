91c346729d144f66b6c9faf9015a7331acce5d59
haikevins <nguyengochai2004@gmail.com>
2026-07-26 23:36:06 +0700

    update: phase11-suspend-resume

--- build/11-task-suspend-resume/kernel/src/hr_list.d
+++
@@ -1,4 +0,0 @@
-build/11-task-suspend-resume/kernel/src/hr_list.o: kernel/src/hr_list.c \
- kernel/internal/hr_list_internal.h kernel/include/hairtos/hr_status.h
-kernel/internal/hr_list_internal.h:
-kernel/include/hairtos/hr_status.h:
--- build/11-task-suspend-resume/kernel/src/hr_timeout.d
+++
@@ -1,9 +0,0 @@
-build/11-task-suspend-resume/kernel/src/hr_timeout.o: \
- kernel/src/hr_timeout.c kernel/internal/hr_timeout_internal.h \
- config/hairtos_config.h kernel/include/hairtos/hr_status.h \
- kernel/include/hairtos/hr_types.h kernel/internal/hr_list_internal.h
-kernel/internal/hr_timeout_internal.h:
-config/hairtos_config.h:
-kernel/include/hairtos/hr_status.h:
-kernel/include/hairtos/hr_types.h:
-kernel/internal/hr_list_internal.h:

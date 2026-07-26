9018de5c13a71a39e6b2eb02bd9bbe189bfacdaf
haikevins <nguyengochai2004@gmail.com>
2026-07-26 22:33:02 +0700

    update: phase8-preemption-and-round-robin

--- build/08-preemption-round-robin/boards/bluepill_f103c8/board_clock.d
+++
@@ -1,5 +0,0 @@
-build/08-preemption-round-robin/boards/bluepill_f103c8/board_clock.o: \
- boards/bluepill_f103c8/board_clock.c \
- boards/bluepill_f103c8/include/board.h soc/stm32f1/include/stm32f1.h
-boards/bluepill_f103c8/include/board.h:
-soc/stm32f1/include/stm32f1.h:
